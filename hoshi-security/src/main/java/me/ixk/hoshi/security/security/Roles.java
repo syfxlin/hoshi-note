1b3f726c9bf5eab57a62d61b1cd55e0599dd4fc5
syfxlin <syfxlin@gmail.com>
2021-05-18 15:54:20 +0800

    :bug: fix: Fix roles valid

--- hoshi-security/src/main/java/me/ixk/hoshi/security/security/Roles.java
+++ hoshi-security/src/main/java/me/ixk/hoshi/security/security/Roles.java
@@ -1,5 +1,9 @@
 package me.ixk.hoshi.security.security;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * @author Otstar Lin
  * @date 2021/5/14 下午 8:33
@@ -20,5 +24,14 @@ public enum Roles {
     /**
      * Spring Boot Admin 访问权限
      */
-    BOOT_ADMIN,
+    BOOT_ADMIN;
+
+    private static final Set<String> ROLES = Arrays
+        .stream(values())
+        .map(Enum::name)
+        .collect(Collectors.toUnmodifiableSet());
+
+    public static boolean contains(final String role) {
+        return ROLES.contains(role);
+    }
 }
--- hoshi-ums/src/main/java/me/ixk/hoshi/ums/entity/AddUserView.java
+++ hoshi-ums/src/main/java/me/ixk/hoshi/ums/entity/AddUserView.java
@@ -1,13 +1,15 @@
 package me.ixk.hoshi.ums.entity;
 
+import java.util.List;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import me.ixk.hoshi.security.entity.Users;
+import me.ixk.hoshi.security.security.Roles;
 
 /**
  * @author Otstar Lin
@@ -18,9 +20,8 @@ import me.ixk.hoshi.security.entity.Users;
 @EqualsAndHashCode(callSuper = true)
 public class AddUserView extends RegisterUserView {
 
-    @Size(max = 100, message = "权限的长度不能超过 100")
     @NotNull(message = "权限不能为空")
-    private String roles;
+    private List<String> roles;
 
     @Min(value = 0, message = "状态值最小不能小于 0")
     @Max(value = 127, message = "状态值最大不能超过 127")
@@ -33,7 +34,7 @@ public class AddUserView extends RegisterUserView {
         final String nickname,
         final String email,
         final String avatar,
-        final String roles,
+        final List<String> roles,
         final Integer status
     ) {
         super(username, password, nickname, email, avatar);
@@ -41,10 +42,15 @@ public class AddUserView extends RegisterUserView {
         this.status = status;
     }
 
+    @AssertTrue(message = "权限不存在")
+    protected boolean isRoleExists() {
+        return this.roles.stream().allMatch(Roles::contains);
+    }
+
     @Override
     public Users toUsers() {
         final Users user = super.toUsers();
-        user.setRoles(this.getRoles());
+        user.setRoles(String.join(",", this.roles));
         user.setStatus(this.getStatus());
         return user;
     }
